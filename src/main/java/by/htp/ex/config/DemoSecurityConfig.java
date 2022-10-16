c6e53eef39ce057267d8d5e5672c705d9d2973d1
Alexandr Drankevich <a.a.drankevich@gmail.com>
2022-10-16 15:20:46 +0300

    1

--- src/main/java/by/htp/ex/config/DemoSecurityConfig.java
+++ src/main/java/by/htp/ex/config/DemoSecurityConfig.java
@@ -6,38 +6,46 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.sql.DataSource;
-
 
 @Configuration
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private DataSource securityDataSource;
+	@Autowired
+	private DataSource securityDataSource;
 
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().dataSource(securityDataSource);
+	}
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication().dataSource(securityDataSource);
-    }
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.addFilterBefore(new EncodingFilter(), ChannelProcessingFilter.class);
+		http.authorizeRequests().antMatchers("/", "/base_page", "/registration/**", "/resources/**").permitAll()
+				.anyRequest().authenticated().and().formLogin().loginPage("/base_page")
+				.loginProcessingUrl("/authenticateTheUser").failureUrl("/base_page?error")
+				.defaultSuccessUrl("/newsList").permitAll().and().logout().logoutSuccessUrl("/").permitAll();
+	}
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/", "/base_page", "/registration/**", "/resources/**")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and().formLogin()
-                .loginPage("/base_page")
-                .loginProcessingUrl("/authenticateTheUser")
-                .failureUrl("/base_page?error")
-                .defaultSuccessUrl("/newsList")
-                .permitAll()
-                .and()
-                .logout()
-                .logoutSuccessUrl("/")
-                .permitAll();
-    }
+	public class EncodingFilter implements Filter {
+
+		@Override
+		public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+				throws IOException, ServletException {
+			request.setCharacterEncoding("UTF-8");
+			response.setContentType("text/html; charset=UTF-8");
+			chain.doFilter(request, response);
+		}
+	}
 }
